a813151a2195540253dc9f5cde0e361c620e2416
Raϕ <72984363+ARyOtaRe@users.noreply.github.com>
2021-12-11 11:49:04 +0100

    Update advent_of_code.py

--- 2021/advent_of_code.py
+++ 2021/advent_of_code.py
@@ -602,75 +602,74 @@ with open("C:\\Users\\ARyOtaRe\\Documents\\GitHub\\Advent-of-Code\\2021\\input_d
     s = f.read()
     
 def parse(s):
-    return [x for x in s.splitlines() if x != '']
+    return [x for x in s.splitlines() if x!='']
 
-v = parse(s)
+v=parse(s)
 
-# part 1
+#part 1
 
-score = 0
+score=0
 
-incomplete = []
+incomplete=[]
 
 for l in v:
-    sck = []
-    good = True
+    sck=[]
+    good=True
     for p in l:
-        if p == '(':
+        if p=='(':
             sck.append('(')
-        elif p == '[':
+        elif p=='[':
             sck.append('[')
-        elif p == '{':
+        elif p=='{':
             sck.append('{')
-        elif p == '<':
+        elif p=='<':
             sck.append('<')
-        elif p == ')':
-            if sck[-1] != '(':
-                score += 3
-                good = False
+        elif p==')':
+            if sck[-1]!='(':
+                score+=3
+                good=False
                 break
-            sck = sck[:-1]
-        elif p == ']':
-            if sck[-1] != '[':
-                score += 57
-                good = False
+            sck=sck[:-1]
+        elif p==']':
+            if sck[-1]!='[':
+                score+=57
+                good=False
                 break
-            sck = sck[:-1]
-        elif p == '}':
-            if sck[-1] != '{':
-                score += 1197
-                good = False
+            sck=sck[:-1]
+        elif p=='}':
+            if sck[-1]!='{':
+                score+=1197
+                good=False
                 break
-            sck = sck[:-1]
-        elif p == '>':
-            if sck[-1] != '<':
-                score += 25137
-                good = False
+            sck=sck[:-1]
+        elif p=='>':
+            if sck[-1]!='<':
+                score+=25137
+                good=False
                 break
-            sck = sck[:-1]
+            sck=sck[:-1]
     if good:
         incomplete.append((l, sck))
 
 print(score)
 
 # part 2
-# setup from part 1 with incomplete list
 
-scores = []
+scores=[]
 
 for l, sck in incomplete:
-    i = len(sck)-1
-    scr = 0
-    while i >= 0:
-        if sck[i] == '(':
-            scr = scr * 5 + 1
-        elif sck[i] == '[':
-            scr = scr * 5 + 2
-        elif sck[i] == '{':
-            scr = scr * 5 + 3
-        elif sck[i] == '<':
-            scr = scr * 5 + 4
-        i -= 1
+    i=len(sck)-1
+    scr=0
+    while i>=0:
+        if sck[i]=='(':
+            scr=scr*5+1
+        elif sck[i]=='[':
+            scr=scr*5+2
+        elif sck[i]=='{':
+            scr=scr*5+3
+        elif sck[i]=='<':
+            scr=scr*5+4
+        i-=1
     scores.append(scr)
 
 scores.sort()
